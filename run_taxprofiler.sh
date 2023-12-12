bdd782bfce45946da0fced21974b3e7b59a5e558
Marieme Top <49489183+Maryemt@users.noreply.github.com>
2023-12-12 16:57:52 +0000

    Add run specifications

--- run_taxprofiler.sh
+++ run_taxprofiler.sh
@@ -4,8 +4,9 @@ Help()
 {
    echo "This project is an automation script to run the pipeline nf-core/taxprofiler pipeline "
    echo
-   echo "Usage : bash nf-core/taxprofiler.sh " directory/ TOOL1[TOOL1,TOOL2..]
+   echo "Usage :./run_taxprofiler.sh " directory/ profilers_list[TOOL1,TOOL2..] <path-to-host-reference-genome>
    echo   
+   echo "Example: ./run_taxprofiler.sh  data/ kraken2,centrifuge /home/db/human_ref.fna"
 }
 
 # Get arguments 
@@ -13,23 +14,24 @@ directory=$1
 tools=$2
 host_ref=$3
 
-ls -v $directory*.fastq.gz > datas_file
+echo "===============
+
+STEP I: GENERATION OF NECESSARY FILES FOR TAXPROFILER: samplesheet.csv & database.csv
+
+==============="
 
 # samplesheet.csv creation
-echo "Generating samplesheet.csv..."
-python3 ./scripts/Samplesheet_generator.py -i datas_file -o samplesheet.csv -t 'I'
+echo "--- Generating samplesheet.csv..."
+ls -v $directory*.fastq.gz > list_file
+python3 ./scripts/Samplesheet_generator.py -i list_file -o samplesheet.csv -t 'I'
+
 
 # database file creation
-echo "Generating database.csv..."
+echo "--- Generating database.csv..."
 python3 ./scripts/databases_generator.py -t $tools -d database.csv  
 tc=$(cat pip.txt)
 
-# Run the pipeline
-#./nextflow run nf-core/taxprofiler --input samplesheet.csv --databases database.csv --outdir ./ -profile docker $tc-resume
 
-# add host removal option 
-# would need to specify the host: here human
-# activate krona reporting 
 echo "===============
 Running nf-core/taxprofiler with the following options:
 repository containing fastq files: $directory
@@ -37,11 +39,36 @@ profilers selected : $tools
 host reference: $host_ref
 ==============="
 
-nextflow run  nf-core/taxprofiler --input samplesheet.csv --databases database.csv --outdir ./ -profile docker $tc-resume --run_krona --perform_shortread_hostremoval --hostremoval_reference $host_ref
+# NOTES: add host removal option 
+
+nextflow run  nf-core/taxprofiler -profile docker $tc-resume \
+--input samplesheet.csv \
+--databases database.csv \
+--outdir taxprofiler_results \
+--run_krona \  # activate krona reporting 
+--perform_shortread_hostremoval \
+--hostremoval_reference $host_ref \ 
+--run_profile_standardisation \ # standardisation of taxon tables across profilers using taxpasta
+--save_hostremoval_unmapped # save reads that did not map to the host genome
 
 # Data management
-rm -f pip.txt
-rm -f datas_file
+rm -f pip.txt list_file
+
+echo "---------
+Command executed :
+nextflow run  nf-core/taxprofiler -profile docker $tc-resume \
+--input samplesheet.csv \
+--databases database.csv \
+--outdir taxprofiler_results \
+--run_krona \   
+--perform_shortread_hostremoval \
+--hostremoval_reference $host_ref \ 
+--run_profile_standardisation \
+--save_hostremoval_unmapped
+
+Output in : taxprofiler_results
+BYE ! 
+"
 
 while getopts ":h" option; do
    case $option in
